e1f3b7757aa84e6465b50c6f66f08781a3a75cd3
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-09-05 14:25:53 -0300

    test: create sigup tests and modify clearDatabase method

--- tests/integration/users.test.ts
+++ tests/integration/users.test.ts
@@ -2,6 +2,7 @@ import "../../src/setup";
 import supertest from "supertest";
 import { getConnection } from "typeorm";
 import app, { init } from "../../src/app";
+import faker from "faker";
 
 import * as usersFactory from "../factories/usersFactory";
 import { clearDatabase } from "../utils/database";
@@ -18,4 +19,73 @@ beforeEach(async () => {
 
 afterAll(async () => {
   await getConnection().close();
+});
+
+describe("POST /sign-up", () => {
+  it("should answer status 201 for a new user registered", async () => {
+    const user = {
+      email: faker.internet.email(),
+      name: faker.name.firstName(),
+      password: "123456",
+      confirmPassword: "123456",
+    };
+    const response = await test.post("/sign-up").send(user);
+    expect(response.status).toBe(201);
+  });
+
+  it("should answer status 409 for an email already registered", async () => {
+    const user = await usersFactory.createUser();
+    const newUser = {
+      email: user.email,
+      name: faker.name.firstName(),
+      password: "456789",
+      confirmPassword: "456789",
+    };
+    const response = await test.post("/sign-up").send(newUser);
+    expect(response.status).toBe(409);
+  });
+
+  it("should answer status 400 for a missing email", async () => {
+    const user = {
+      email: "",
+      name: faker.name.firstName(),
+      password: "123456",
+      confirmPassword: "123456",
+    };
+    const response = await test.post("/sign-up").send(user);
+    expect(response.status).toBe(400);
+  });
+
+  it("should answer status 400 for a missing password", async () => {
+    const user = {
+      email: "test@test.com",
+      name: faker.name.firstName(),
+      password: "",
+      confirmPassword: "",
+    };
+    const response = await test.post("/sign-up").send(user);
+    expect(response.status).toBe(400);
+  });
+
+  it("should answer status 400 for an invalid password confirmation", async () => {
+    const user = {
+      email: "test@test.com",
+      name: faker.name.firstName(),
+      password: "123456",
+      confirmPassword: "1233555",
+    };
+    const response = await test.post("/sign-up").send(user);
+    expect(response.status).toBe(400);
+  });
+
+  it("should answer status 400 for invalid email", async () => {
+    const user = {
+      email: "test.emai.com",
+      name: faker.name.firstName(),
+      password: "123456",
+      confirmPassword: "123456",
+    };
+    const response = await test.post("/sign-up").send(user);
+    expect(response.status).toBe(400);
+  });
 });
