100b55a541a3f453c8e91342292e9bace891d80e
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-09-06 02:57:53 -0300

    fix: inserting middlewares in app file

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,17 @@ import connectDatabase from "./database";
 
 import * as userControllers from "./controllers/client/user";
 import * as authControllers from "./controllers/client/auth";
+import { authMiddleware } from "./middlewares/authMiddleware";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(errorHandler);
 
 app.post("/sign-up", userControllers.signUp);
 app.post("/sign-in", authControllers.signIn);
+app.get("/barbecues", authMiddleware, barbecueControllers.getBarbecues);
 
 export async function init() {
   await connectDatabase();
