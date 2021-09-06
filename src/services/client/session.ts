0fef9ff5e18b450ed941263685fa98546fcf286e
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-09-06 02:54:08 -0300

    refactor: create authentication middleware

---
+++ src/services/client/session.ts
@@ -0,0 +1,7 @@
+import Sessions from "../../entities/Sessions";
+
+export async function findSessionByToken(token: string) {
+  const session = await Sessions.findOne({ where: { token } });
+
+  return session;
+}
